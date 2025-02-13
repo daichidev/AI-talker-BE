a380da81006d8a8eaab0fb2dc3f35ec8944dec6c
daichidev <mentaikomayoyo@gmail.com>
2025-02-13 11:40:39 +0900

    AVATARモデルの追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Anketo;
 use App\Models\User;
+use App\Models\Avatar;
 use App\Http\Controllers\DeepImageController;
 
 class UserController extends Controller
