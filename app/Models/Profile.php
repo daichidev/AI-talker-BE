6a5b0fc723c4f20d85094017f318d1c5e7d8e82e
daichidev <mentaikomayoyo@gmail.com>
2025-05-15 17:22:19 +0900

    Profile モデルの fillable 引数を追加

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'ai_name',
+        'gender',
+        'birthdate',
+        'hometown',
+        'address',
+        'blood_type',
+        'school_name',
+        'company_name',
+        'income_or_allowance',
+        'hobby',
+        'family_structure',
+        'special_skills',
+        'dream',
+        'favorite_type',
+        'weakness',
+        'animal_fortune_telling_result',
+    ];
 }
