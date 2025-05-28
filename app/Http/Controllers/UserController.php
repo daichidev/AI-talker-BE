c6c2af6b9a213ad60db62dee2b99bcdfd31bd3fa
daichidev <mentaikomayoyo@gmail.com>
2025-05-28 12:52:39 +0900

    コメントを日本語に変更

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,7 +40,7 @@ class UserController extends Controller
         if ($existingEmailUser) {
             return response()->json([
                 'success' => false,
-                'message' => 'このメールアドレスは既に使用されています。', // This email is already in use
+                'message' => 'This email is already in use.'
             ]);
         }
         
@@ -49,7 +49,7 @@ class UserController extends Controller
         if ($existingUser) {
             return response()->json([
                 'success' => false,
-                'message' => 'このユーザーは既に登録されています。',
+                'message' => 'This device ID is already in use.'
             ]);  // 重複するデバイスIDの場合、400エラー（不正なリクエスト）を返す
         }
 
