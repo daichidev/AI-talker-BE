2d4b4860c9e00dc436f26a59da43e5d6f6f7ff2f
daichidev <mentaikomayoyo@gmail.com>
2025-06-15 16:48:14 +0900

    Syncroモデルを変更

--- app/Models/Syncro.php
+++ app/Models/Syncro.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Syncro extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'score_profile',
+        'done_animal_fortune',
+        'done_big5_analysis',
+        'done_kakeai',
+        'score_login',
+        'score_ai_talk',
+        'score_friend_invite_sent',
+        'score_friend_invite_received',
+        'done_personality_test',
+        'score_account_link',
+        'score_sns_link',
+        'done_location_info',
+        'done_cookie_on'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
