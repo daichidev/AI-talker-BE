fa1791b01cb1b95ce892d0ddd7a52dfbebedd50b
daichidev <mentaikomayoyo@gmail.com>
2025-06-19 15:07:31 +0900

    ProfileControllerの修正

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Profile;
+use App\Models\Syncro;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController;
 use Carbon\Carbon;
